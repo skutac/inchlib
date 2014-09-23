fc94128678188617b3d6655f8943c87e353242d3
skutac <skuta.ctibor@gmail.com>
2014-09-23 09:34:55 +0200

    Admin for server changed

--- examples/admin.py
+++ examples/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
-from examples.models import Examples, ExampleTypes
+from examples.models import Examples, ExampleTypes, SettingsAttributes, ExampleSettings
 
 
 class ExamplesAdmin(admin.ModelAdmin):
-    # fields = ('title', 'description')
     list_display = ('title', 'exampletype', 'order',)
     fieldsets = (
         (None, {
-        	'classes': ('wide',),
+            'classes': ('wide',),
             'fields': ('title', 'description', 'data', 'exampletype', 'order'),
         }),
     )
@@ -27,3 +26,34 @@ class ExampleTypesAdmin(admin.ModelAdmin):
         }
 
 admin.site.register(ExampleTypes, ExampleTypesAdmin)
+
+class SettingsAttributesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'settingsattributetype')
+    fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('name', 'description', 'settingsattributetype'),
+        }),
+    )
+    class Media:
+        css = {
+            "all": ("css/admin.css",)
+        }
+
+admin.site.register(SettingsAttributes, SettingsAttributesAdmin)
+
+
+class ExampleSettingsAdmin(admin.ModelAdmin):
+    list_display = ('example', 'settingsattribute', 'value')
+    fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('example', 'settingsattribute', 'value'),
+        }),
+    )
+    class Media:
+        css = {
+            "all": ("css/admin.css",)
+        }
+
+admin.site.register(ExampleSettings, ExampleSettingsAdmin)
